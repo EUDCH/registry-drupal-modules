6f9f3ccc060828f1403dbe6b8698de2321cd8a18
Baptiste Grenier <baptiste@bapt.name>
2026-04-14 23:20:12 +0200

    Revert "style: fix JavaScript style"
    This reverts commit 19eadf04f9c9aa31440d30114739a70e2d2437c9.

--- organization_validation/js/toggle_rows.js
+++ organization_validation/js/toggle_rows.js
@@ -1,50 +1,47 @@
 (function ($, Drupal, once) {
-  Drupal.behaviors.toggleRows = {
-    attach: function (context) {
-      once(
-        "radioSelection",
-        $('input[name="selected_organisation"]', context),
-      ).forEach((radio) => {
-        $(radio).on("change", function () {
-          if ($('input[name="selected_organisation"]:checked').length > 0) {
-            $(".action-button-2").fadeIn(); // Show button
-          } else {
-            $(".action-button-2").fadeOut(); // Hide button
-          }
-        });
-      });
-    },
-  };
+    Drupal.behaviors.toggleRows = {
+        attach: function (context) {
+            once('radioSelection', $('input[name="selected_organisation"]', context)).forEach((radio) => {
+                $(radio).on('change', function () {
+                    if ($('input[name="selected_organisation"]:checked').length > 0) {
+                        $('.action-button-2').fadeIn(); // Show button
+                    } else {
+                        $('.action-button-2').fadeOut(); // Hide button
+                    }
+                });
+            });
+        }
+    };
 })(jQuery, Drupal, once);
 
 // ✅ Function to send selected organisation to OrganizationValidationController
 // eslint-disable-next-line no-unused-vars
 function submitSelectedOrganisation() {
-  console.log("test");
-  let selectedRadio = $('input[name="selected_organisation"]:checked');
+    console.log("test");
+    let selectedRadio = $('input[name="selected_organisation"]:checked');
 
-  if (selectedRadio.length === 0) {
-    alert("Please select an organisation first.");
-    return;
-  }
+    if (selectedRadio.length === 0) {
+        alert("Please select an organisation first.");
+        return;
+    }
 
-  let orgData = selectedRadio.val();
+    let orgData = selectedRadio.val();
 
-  console.log(orgData);
+    console.log(orgData);
 
-  $.ajax({
-    url: "/organization-validation/manageSelectedOrganisations", // Route to controller
-    type: "POST",
-    data: { organisation_data: orgData }, // Send combined data
-    success: function (response) {
-      if (response.redirect) {
-        window.location.href = response.redirect; // Redirect on success
-      } else {
-        alert("Submission successful, but no redirect URL was provided.");
-      }
-    },
-    error: function () {
-      alert("Error processing your request.");
-    },
-  });
+    $.ajax({
+        url: '/organization-validation/manageSelectedOrganisations', // Route to controller
+        type: 'POST',
+        data: { organisation_data: orgData }, // Send combined data
+        success: function (response) {
+            if (response.redirect) {
+                window.location.href = response.redirect; // Redirect on success
+            } else {
+                alert("Submission successful, but no redirect URL was provided.");
+            }
+        },
+        error: function () {
+            alert("Error processing your request.");
+        }
+    });
 }
